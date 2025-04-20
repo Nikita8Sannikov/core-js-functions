84e8dea476d763e88e96d6fbe0147c76301a6693
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-04-21 01:22:53 +0600

    feat: add getIdGeneratorFunction and partialUsingArguments solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -191,8 +191,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function partial(...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
@@ -212,8 +214,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  const idGenerator = { value: startFrom };
+  return function getId() {
+    const currentValue = idGenerator.value;
+    idGenerator.value += 1;
+    return currentValue;
+  };
 }
 
 module.exports = {
